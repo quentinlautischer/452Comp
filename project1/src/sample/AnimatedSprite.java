ce89adab8c2a44c0f5aec6b7bb1a3b893e18e0cd
Quentin Lautischer <lautisch@ualberta.ca>
2017-10-10 09:28:02 -0600

    init

---
+++ project1/src/sample/AnimatedSprite.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+
+public class AnimatedSprite extends Sprite {
+
+    private ArrayList<Image> images;
+
+    public AnimatedSprite()
+    {
+        super();
+    }
+
+    public void update(double time)
+    {
+        super.update(time);
+
+    }
+
+}
